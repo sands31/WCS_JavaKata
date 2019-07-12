8d7f953293041394a18f04244660996e4abd4009
bastienwcs <email>
2019-07-12 15:12:19 +0200

    solution

--- src/kata/HelloYou.java
+++ src/kata/HelloYou.java
@@ -1,4 +1,9 @@
 public class HelloYou {
 
-    // TODO
+    public static String sayHello(String firstName) {
+        if (firstName == null || firstName.equals("")) {
+            firstName = "World";
+        }
+        return "Hello " + firstName;
+    }
 }
--- src/test/HelloYouTest.java
+++ src/test/HelloYouTest.java
@@ -7,4 +7,19 @@ public class HelloYouTest {
     public void testTrue() throws Exception {
         Assert.assertEquals(true, true);
     }
+
+    @Test
+    public void testNull() throws Exception {
+        Assert.assertEquals("Hello World", HelloYou.sayHello(null));
+    }
+
+    @Test
+    public void testEmpty() throws Exception {
+        Assert.assertEquals("Hello World", HelloYou.sayHello(""));
+    }
+
+    @Test
+    public void testBarbara() throws Exception {
+        Assert.assertEquals("Hello Barbara", HelloYou.sayHello("Barbara"));
+    }
 }
