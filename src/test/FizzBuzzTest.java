a235fbfd1b6943f13d185c36428f156700e6b6b7
bastienwcs <email>
2019-07-12 15:37:15 +0200

    solution

--- src/test/FizzBuzzTest.java
+++ src/test/FizzBuzzTest.java
@@ -8,5 +8,23 @@ public class FizzBuzzTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testN() throws Exception {
+        Assert.assertEquals("-1", FizzBuzz.fizzBuzz(-1));
+    }
+
+    @Test
+    public void test6() throws Exception {
+        Assert.assertEquals("Fizz", FizzBuzz.fizzBuzz(6));
+    }
+
+    @Test
+    public void test10() throws Exception {
+        Assert.assertEquals("Buzz", FizzBuzz.fizzBuzz(10));
+    }
+
+    @Test
+    public void test30() throws Exception {
+        Assert.assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(30));
+    }
 }
