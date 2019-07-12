2287f329b686098e3023b2601a8f194010764542
bastienwcs <email>
2019-07-12 11:28:56 +0200

    init

---
+++ src/test/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testTrue() throws Exception {
+        Assert.assertEquals(true, true);
+    }
+}
