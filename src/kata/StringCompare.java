c0650e7689ee538e7f28b0103af780200839a371
bastienwcs <bastien@wildcodeschool.fr>
2019-07-13 08:43:39 +0200

    solution

--- src/kata/StringCompare.java
+++ src/kata/StringCompare.java
@@ -1,4 +1,16 @@
 public class StringCompare {
 
-    // TODO
+    public static int compare(String origin, String compare) {
+        if (origin == null || compare == null ||
+            origin.length() != compare.length()) {
+            return -1;
+        }
+        int count = 0;
+        for (int i = 0; i < origin.length(); i++) {
+            if (origin.charAt(i) == compare.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- src/test/StringCompareTest.java
+++ src/test/StringCompareTest.java
@@ -8,5 +8,21 @@ public class StringCompareTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testNull() throws Exception {
+        Assert.assertEquals(-1, StringCompare.compare(null, null));
+    }
+
+    @Test
+    public void testLength() throws Exception {
+        Assert.assertEquals(-1, StringCompare.compare("tacos", "poulpe"));
+    }
+
+    @Test
+    public void testOther() throws Exception {
+        Assert.assertEquals(1, StringCompare.compare("a", "a"));
+        Assert.assertEquals(0, StringCompare.compare("a", "b"));
+        Assert.assertEquals(1, StringCompare.compare("aa", "ba"));
+        Assert.assertEquals(3, StringCompare.compare("cassis", "castor"));
+    }
 }
