37c9e2f013ff7822734a437a757a0cde3e982b2a
bastienwcs <bastien@wildcodeschool.fr>
2019-07-13 08:32:06 +0200

    solution

--- src/kata/CountLetters.java
+++ src/kata/CountLetters.java
@@ -1,4 +1,15 @@
 public class CountLetters {
 
-    // TODO
+    public static int count(String str, char c) {
+        if (str == null) {
+            return -1;
+        }
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- src/test/CountLettersTest.java
+++ src/test/CountLettersTest.java
@@ -8,5 +8,16 @@ public class CountLettersTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testNull() throws Exception {
+        Assert.assertEquals(-1, CountLetters.count(null, 'a'));
+    }
+
+    @Test
+    public void testOther() throws Exception {
+        Assert.assertEquals(1, CountLetters.count("a", 'a'));
+        Assert.assertEquals(7, CountLetters.count("aaaaabbbaa", 'a'));
+        Assert.assertEquals(1, CountLetters.count("bbacbaaa", 'c'));
+        Assert.assertEquals(0, CountLetters.count("bbcc", 'a'));
+    }
 }
