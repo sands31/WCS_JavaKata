06c5a5478abc47db4f8f0c3787b83665512c069b
bastienwcs <bastien@wildcodeschool.fr>
2019-07-13 13:06:49 +0200

    solution

--- src/kata/RomanToArabic.java
+++ src/kata/RomanToArabic.java
@@ -1,4 +1,41 @@
+import java.util.HashMap;
+
 public class RomanToArabic {
 
-    // TODO
+    public static int convert(String roman) {
+        if (roman == null || roman.isBlank()) {
+            throw new IllegalArgumentException("Illegal roman numeral");
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+        for(int i = 0; i < roman.length(); i++) {
+            if (!map.containsKey(roman.charAt(i))) {
+                throw new IllegalArgumentException("Illegal roman numeral");
+            }
+            int current = map.get(roman.charAt(i));
+            if (i + 1 < roman.length()) {
+                if (!map.containsKey(roman.charAt(i + 1))) {
+                    throw new IllegalArgumentException("Illegal roman numeral");
+                }
+                int next = map.get(roman.charAt(i + 1));
+                if (current < next) {
+                    result -= current;
+                } else {
+                    result += current;
+                }
+            } else {
+                result += current;
+            }
+        }
+        return result;
+    }
 }
--- src/test/RomanToArabicTest.java
+++ src/test/RomanToArabicTest.java
@@ -8,5 +8,48 @@ public class RomanToArabicTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testEmpty() throws Exception {
+        try {
+            RomanToArabic.convert("");
+            Assert.fail("No IllegalArgumentException throw for empty argument");
+        } catch (IllegalArgumentException e) {
+            Assert.assertEquals("Illegal roman numeral", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testNull() throws Exception {
+        try {
+            RomanToArabic.convert(null);
+            Assert.fail("No IllegalArgumentException throw for null argument");
+        } catch (IllegalArgumentException e) {
+            Assert.assertEquals("Illegal roman numeral", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testIllegal() throws Exception {
+        try {
+            RomanToArabic.convert("XS");
+            Assert.fail("No IllegalArgumentException throw for illegal argument");
+        } catch (IllegalArgumentException e) {
+            Assert.assertEquals("Illegal roman numeral", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testOther() throws Exception {
+        Assert.assertEquals(1, RomanToArabic.convert("I"));
+        Assert.assertEquals(3, RomanToArabic.convert("III"));
+        Assert.assertEquals(4, RomanToArabic.convert("IV"));
+        Assert.assertEquals(5, RomanToArabic.convert("V"));
+        Assert.assertEquals(8, RomanToArabic.convert("VIII"));
+        Assert.assertEquals(9, RomanToArabic.convert("IX"));
+        Assert.assertEquals(10, RomanToArabic.convert("X"));
+        Assert.assertEquals(89, RomanToArabic.convert("IXC"));
+        Assert.assertEquals(111, RomanToArabic.convert("CXI"));
+        Assert.assertEquals(666, RomanToArabic.convert("DCLXVI"));
+        Assert.assertEquals(1000, RomanToArabic.convert("M"));
+    }
 }
