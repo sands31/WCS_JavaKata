f8c40387c96ba7b02758f9a7365787efc660c55b
bastienwcs <email>
2019-07-15 09:39:30 +0200

    solution

--- src/kata/RecursiveMultiply.java
+++ src/kata/RecursiveMultiply.java
@@ -1,4 +1,17 @@
 public class RecursiveMultiply {
 
-    // TODO
+    public static long multiply(int a, int b) {
+        if (Math.abs(b) > Math.abs(a)) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        if (b == 0) {
+            return 0;
+        }
+        if (b < 0) {
+            return -(a + multiply(a, -b - 1));
+        }
+        return a + multiply(a, b - 1);
+    }
 }
--- src/test/RecursiveMultiplyTest.java
+++ src/test/RecursiveMultiplyTest.java
@@ -8,5 +8,24 @@ public class RecursiveMultiplyTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testZero() throws Exception {
+        Assert.assertEquals(0, RecursiveMultiply.multiply(3, 0));
+        Assert.assertEquals(0, RecursiveMultiply.multiply(0, 5));
+        Assert.assertEquals(0, RecursiveMultiply.multiply(0, 0));
+    }
+
+    @Test
+    public void testPositive() throws Exception {
+        Assert.assertEquals(18, RecursiveMultiply.multiply(3, 6));
+        Assert.assertEquals(12, RecursiveMultiply.multiply(6, 2));
+        Assert.assertEquals(1000000, RecursiveMultiply.multiply(100000, 10));
+    }
+
+    @Test
+    public void testNegative() throws Exception {
+        Assert.assertEquals(-18, RecursiveMultiply.multiply(3, -6));
+        Assert.assertEquals(-12, RecursiveMultiply.multiply(-6, 2));
+        Assert.assertEquals(24, RecursiveMultiply.multiply(-6, -4));
+    }
 }
