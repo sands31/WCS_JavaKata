5c1c084444a3e3936fb9836911bb0c8aa6ad96a5
bastienwcs <email>
2019-07-16 17:44:52 +0200

    solution

--- src/kata/SortArray.java
+++ src/kata/SortArray.java
@@ -1,4 +1,17 @@
 public class SortArray {
 
-    // TODO
+    public static int[] sort(int[] values) {
+
+        for (int i = 0; i < values.length; i++) {
+            int value = values[i];
+            for (int j = i; j < values.length; j++) {
+                if (values[j] < value) {
+                    value = values[j];
+                    values[j] = values[i];
+                    values[i] = value;
+                }
+            }
+        }
+        return values;
+    }
 }
--- src/test/SortArrayTest.java
+++ src/test/SortArrayTest.java
@@ -8,5 +8,11 @@ public class SortArrayTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testSort() throws Exception {
+        Assert.assertArrayEquals(new int[]{}, SortArray.sort(new int[]{}));
+        Assert.assertArrayEquals(new int[]{1, 2, 3, 4, 5}, SortArray.sort(new int[]{5, 2, 3, 1, 4}));
+        Assert.assertArrayEquals(new int[]{-16, -4, 3, 6, 7}, SortArray.sort(new int[]{-16, 6, 3, -4, 7}));
+        Assert.assertArrayEquals(new int[]{Integer.MIN_VALUE, 0, Integer.MAX_VALUE}, SortArray.sort(new int[]{0, Integer.MAX_VALUE, Integer.MIN_VALUE}));
+    }
 }
