773d4d6483ec783716e3c65640737617bfe060f3
bastienwcs <email>
2019-07-16 15:13:11 +0200

    fixes

--- src/kata/Arena.java
+++ src/kata/Arena.java
@@ -2,26 +2,7 @@ public class Arena {
 
     public static String fight(Worm worm1, Worm worm2) {
 
-        while (worm1.getLife() > 0 && worm2.getLife() > 0) {
-            if (worm1.getLife() >= worm2.getLife()) {
-                attack(worm1, worm2);
-                if (worm2.getLife() > 0) {
-                    attack(worm2, worm1);
-                }
-            } else {
-                attack(worm2, worm1);
-                if (worm1.getLife() > 0) {
-                    attack(worm1, worm2);
-                }
-            }
-        }
-
-        return worm1.getLife() > 0 ? worm1.getName() : worm2.getName();
-    }
-
-    private static void attack(Worm worm1, Worm worm2) {
-        System.out.println(worm1.getName() + " attacks " + worm2.getName());
-        worm2.setLife(worm2.getLife() - worm1.getAttack());
-        System.out.println(worm2.getName() + " has " + worm2.getLife() + " remaining");
+        // TODO
+        return null;
     }
 }
--- src/kata/Worm.java
+++ src/kata/Worm.java
@@ -1,36 +1,8 @@
 public class Worm {
 
-    private String name;
-    private int life;
-    private int attack;
+    // TODO
 
     public Worm(String name, int life, int attack) {
-        this.name = name;
-        this.life = life;
-        this.attack = attack;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getLife() {
-        return life;
-    }
-
-    public void setLife(int life) {
-        this.life = life;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    public void setAttack(int attack) {
-        this.attack = attack;
+        // TODO
     }
 }
