b880d4f8799c7ab05f3213664de35ae49c056083
bastienwcs <email>
2019-07-16 16:33:03 +0200

    solution

--- src/kata/CountMinors.java
+++ src/kata/CountMinors.java
@@ -1,4 +1,15 @@
 public class CountMinors {
 
-    // TODO
+    public static int count(int[] ages, int legalAge) {
+        if (ages == null || legalAge < 0) {
+            return -1;
+        }
+        int count = 0;
+        for (int age : ages) {
+            if (age < legalAge) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- src/test/CountMinorsTest.java
+++ src/test/CountMinorsTest.java
@@ -8,5 +8,19 @@ public class CountMinorsTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testNull() throws Exception {
+        Assert.assertEquals(-1, CountMinors.count(null, 0));
+    }
+
+    @Test
+    public void testNegative() throws Exception {
+        Assert.assertEquals(-1, CountMinors.count(new int[]{18}, -1));
+    }
+
+    @Test
+    public void testCount() throws Exception {
+        Assert.assertEquals(4, CountMinors.count(new int[]{12, 16, 23, 47, 5, 16, 18}, 18));
+        Assert.assertEquals(5, CountMinors.count(new int[]{12, 16, 23, 47, 5, 16, 18}, 21));
+    }
 }
