db8efda01f741877a366c6beaf483519ae70c097
bastienwcs <email>
2019-07-16 16:14:39 +0200

    solution

--- src/kata/HexToRGB.java
+++ src/kata/HexToRGB.java
@@ -1,4 +1,14 @@
 public class HexToRGB {
 
-    // TODO
+    public static int[] converts(String webColor) {
+        String red = webColor.substring(1, 3);
+        String blue = webColor.substring(3, 5);
+        String green = webColor.substring(5, 7);
+
+        return new int[]{
+                Integer.parseInt(red, 16),
+                Integer.parseInt(blue, 16),
+                Integer.parseInt(green, 16)
+        };
+    }
 }
--- src/test/HexToRGBTest.java
+++ src/test/HexToRGBTest.java
@@ -8,5 +8,10 @@ public class HexToRGBTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testConvert() throws Exception {
+        Assert.assertArrayEquals(new int[]{0, 0, 0}, HexToRGB.converts("#000000"));
+        Assert.assertArrayEquals(new int[]{255, 215, 0}, HexToRGB.converts("#FFD700"));
+        Assert.assertArrayEquals(new int[]{154, 205, 50}, HexToRGB.converts("#9ACD32"));
+    }
 }
