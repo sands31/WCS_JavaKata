9c27e80a086a139d691269716cbb233d06988d14
bastienwcs <email>
2019-07-16 14:10:41 +0200

    solution

--- src/kata/Minesweeper.java
+++ src/kata/Minesweeper.java
@@ -1,4 +1,29 @@
 public class Minesweeper {
 
-    // TODO
+    public static String sweep(int[][] grid, int row, int column) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            throw new IllegalArgumentException();
+        }
+        if (row < 0 || row >= grid.length
+                || column < 0 || column >= grid[row].length) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (grid[row][column] == 1) {
+            return "kaboom";
+        }
+        int minRow = Math.max(row - 1, 0);
+        int maxRow = Math.min(row + 1, grid.length - 1);
+        int minColumn = Math.max(column - 1, 0);
+        int maxColumn = Math.min(column + 1, grid[row].length - 1);
+
+        int bombs = 0;
+        for (int i = minRow; i <= maxRow; i++) {
+            for (int j = minColumn; j <= maxColumn; j++) {
+                if (grid[i][j] == 1) {
+                    bombs++;
+                }
+            }
+        }
+        return "" + bombs;
+    }
 }
--- src/test/MinesweeperTest.java
+++ src/test/MinesweeperTest.java
@@ -8,5 +8,58 @@ public class MinesweeperTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullArgument() throws Exception {
+        Minesweeper.sweep(null, 1, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIllegalArgument() throws Exception {
+        int[][] grid = {{}};
+        Minesweeper.sweep(grid, 0, 3);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testOutOfBoundRow() throws Exception {
+        int[][] grid = {
+                {0, 0, 0, 1},
+                {0, 1, 0, 1},
+                {1, 0, 0, 0}
+        };
+        Minesweeper.sweep(grid, 4, 3);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testOutOfBoundColumn() throws Exception {
+        int[][] grid = {
+                {0, 0, 0, 1},
+                {0, 1, 0, 1},
+                {1, 0, 0, 0}
+        };
+        Minesweeper.sweep(grid, 0, -1);
+    }
+
+    @Test
+    public void testBoom() throws Exception {
+        int[][] grid = {
+                {0, 0, 0, 1},
+                {0, 1, 0, 1},
+                {1, 0, 0, 0}
+        };
+        Assert.assertEquals("kaboom", Minesweeper.sweep(grid, 1, 1));
+        Assert.assertEquals("kaboom", Minesweeper.sweep(grid, 0, 3));
+        Assert.assertEquals("kaboom", Minesweeper.sweep(grid, 2, 0));
+    }
+
+    @Test
+    public void testSweep() throws Exception {
+        int[][] grid = {
+                {0, 0, 0, 1},
+                {0, 1, 0, 0},
+                {1, 1, 0, 0}
+        };
+        Assert.assertEquals("1", Minesweeper.sweep(grid, 0, 0));
+        Assert.assertEquals("3", Minesweeper.sweep(grid, 1, 2));
+        Assert.assertEquals("0", Minesweeper.sweep(grid, 2, 3));
+    }
 }
