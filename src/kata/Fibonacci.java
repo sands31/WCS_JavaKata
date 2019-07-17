5e91e084bd59361d27b15ef5de57414306898631
bastienwcs <email>
2019-07-17 11:00:40 +0200

    solution

--- src/kata/Fibonacci.java
+++ src/kata/Fibonacci.java
@@ -1,4 +1,10 @@
 public class Fibonacci {
 
-    // TODO
+    public static int fibo(int n) {
+        if (n <= 1) {
+            return n; // fibo(0) == 0 and fibo(1) == 1
+        } else {
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    }
 }
--- src/test/FibonacciTest.java
+++ src/test/FibonacciTest.java
@@ -8,5 +8,12 @@ public class FibonacciTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testFibo() throws Exception {
+        Assert.assertEquals(0, Fibonacci.fibo(0));
+        Assert.assertEquals(1, Fibonacci.fibo(1));
+        Assert.assertEquals(3, Fibonacci.fibo(4));
+        Assert.assertEquals(8, Fibonacci.fibo(6));
+        Assert.assertEquals(21, Fibonacci.fibo(8));
+    }
 }
