734e145a2cc4c58e4c22200c5c4294edd0efc9f8
bastienwcs <email>
2019-07-17 13:42:04 +0200

    solution

--- src/kata/Compression.java
+++ src/kata/Compression.java
@@ -1,4 +1,27 @@
 public class Compression {
 
-    // TODO
+    public static String encode(String data) {
+        if (data == null) {
+            return "";
+        }
+        String result = "";
+        int count = 1;
+
+        for (int i = 0; i < data.length(); i++) {
+            char current = data.charAt(i);
+            if (i + 1 == data.length() || current != data.charAt(i + 1)) {
+                result += current;
+                if (count == 2) {
+                    result += current;
+                } else if (count > 2) {
+                    result += count;
+                }
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+
+        return result;
+    }
 }
--- src/test/CompressionTest.java
+++ src/test/CompressionTest.java
@@ -8,5 +8,15 @@ public class CompressionTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testEncode() throws Exception {
+        Assert.assertEquals("", Compression.encode(null));
+        Assert.assertEquals("", Compression.encode(""));
+        Assert.assertEquals("a", Compression.encode("a"));
+        Assert.assertEquals("aa", Compression.encode("aa"));
+        Assert.assertEquals("a3", Compression.encode("aaa"));
+        Assert.assertEquals("a4bbc3a", Compression.encode("aaaabbccca"));
+        Assert.assertEquals("tacos", Compression.encode("tacos"));
+        Assert.assertEquals("abbc3a3", Compression.encode("abbcccaaa"));
+    }
 }
