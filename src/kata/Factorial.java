fbe22f2cb5d582ad07c0e7279a64eaca958658ac
bastienwcs <email>
2019-07-19 17:36:48 +0200

    solution

--- src/kata/Factorial.java
+++ src/kata/Factorial.java
@@ -1,4 +1,9 @@
 public class Factorial {
 
-    // TODO
+    public static long facto(long n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return facto(n - 1) * n;
+    }
 }
--- src/test/FactorialTest.java
+++ src/test/FactorialTest.java
@@ -8,5 +8,12 @@ public class FactorialTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testFactorial() throws Exception {
+        Assert.assertEquals(1, Factorial.facto(0));
+        Assert.assertEquals(1, Factorial.facto(1));
+        Assert.assertEquals(2, Factorial.facto(2));
+        Assert.assertEquals(6, Factorial.facto(3));
+        Assert.assertEquals(24, Factorial.facto(4));
+    }
 }
