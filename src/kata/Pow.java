83dc4923cecac7d25f052254d6c6f243acb3d940
bastienwcs <email>
2019-07-19 17:51:45 +0200

    solution

--- src/kata/Pow.java
+++ src/kata/Pow.java
@@ -1,4 +1,9 @@
 public class Pow {
 
-    // TODO
+    public static double pow(int n, int e) {
+        if (e < 0) {
+            return 1 / pow(n, -e);
+        }
+        return e == 0 ? 1 : n * pow(n, e - 1);
+    }
 }
--- src/test/PowTest.java
+++ src/test/PowTest.java
@@ -8,5 +8,14 @@ public class PowTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testPow() throws Exception {
+        Assert.assertEquals(1, Pow.pow(2, 0), 0);
+        Assert.assertEquals(2, Pow.pow(2, 1), 0);
+        Assert.assertEquals(4, Pow.pow(2, 2), 0);
+        Assert.assertEquals(8, Pow.pow(2, 3), 0);
+        Assert.assertEquals(16, Pow.pow(2, 4), 0);
+        Assert.assertEquals(32, Pow.pow(2, 5), 0);
+        Assert.assertEquals(0.25, Pow.pow(2, -2), 0);
+    }
 }
