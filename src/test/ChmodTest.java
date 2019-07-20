383222537765f685357a5af3ed7f99f84495179e
bastienwcs <bastien@wildcodeschool.fr>
2019-07-20 10:41:30 +0200

    solution

--- src/test/ChmodTest.java
+++ src/test/ChmodTest.java
@@ -8,5 +8,10 @@ public class ChmodTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testConversion() throws Exception {
+        Assert.assertEquals("---------", Chmod.convert("000"));
+        Assert.assertEquals("-wxr-x-w-", Chmod.convert("352"));
+        Assert.assertEquals("rwxrwxrwx", Chmod.convert("777"));
+    }
 }
