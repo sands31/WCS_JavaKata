804f6a08a6a74526ad3cd889717c2737804383e9
Sandra Roques <roquesandra@hotmail.fr>
2019-12-05 12:01:37 +0100

    CaesarCipher

--- src/kata/CaesarCipher.java
+++ src/kata/CaesarCipher.java
@@ -1,4 +1,24 @@
 public class CaesarCipher {
 
-    // TODO
+    public static String ceasarCipher(String word, int shift) {
+    	String encrypt = "";
+    	int encryptInt;
+    	for (int i = 0; i < word.length(); i++) {
+    		encryptInt = (word.charAt(i) + shift);
+    		if (encryptInt < 97)
+    			encryptInt += 26;
+    		if (encryptInt > 122)
+    			encryptInt -= 26;
+    		
+    		encrypt += (char) encryptInt;
+    	} 	  	
+		return encrypt; 	
+    }
+    
+//    public static void main(String[] args) {
+//    	System.out.println(ceasarCipher("abcd", 1));
+//    	System.out.println(ceasarCipher("abcd", -1));
+//    	System.out.println(ceasarCipher("tacos", 3));
+//    	System.out.println(ceasarCipher("zebra", 2));
+//    }
 }
--- src/test/CaesarCipherTest.java
+++ src/test/CaesarCipherTest.java
@@ -5,8 +5,21 @@ public class CaesarCipherTest {
 
     @Test
     public void testTrue() throws Exception {
-        Assert.assertEquals(true, true);
+        Assert.assertEquals("bcde", CaesarCipher.ceasarCipher("abcd", 1));
     }
-
-    // TODO
+    
+    @Test
+    public void testTrue1() throws Exception {
+        Assert.assertEquals("zabc", CaesarCipher.ceasarCipher("abcd", -1));
+    }
+    
+    @Test
+    public void testTrue2() throws Exception {
+        Assert.assertEquals("wdfrv", CaesarCipher.ceasarCipher("tacos", 3));
+    }
+    
+    @Test
+    public void testTrue3() throws Exception {
+        Assert.assertEquals("bgdtc", CaesarCipher.ceasarCipher("zebra", 2));
+    }
 }
